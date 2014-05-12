8dc022c61192f01d50d9b1433263220b91ab6975
mark reyes <mark.reyes@ucop.edu>
2014-05-12 13:56:29 -0700

    change PID name

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/apps/dash/apps/dash-ingest'
 # server 'dash-dev.cdlib.org', user: 'dash', roles: %w{web app}, my_property: :my_value
 server 'dash-dev.cdlib.org', user: 'dash', roles: %w{web app}
 
-set :unicorn_pid, "#{deploy_to}/unicorn.dash.pid"
+set :unicorn_pid, "#{deploy_to}/unicorn.pid"
 set :unicorn_config, "#{deploy_to}/config/unicorn.rb"
 set :unicorn_port, "8080"
 
