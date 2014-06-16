58ed2c36434775d7b867fdd462c037396d62a535
Shirin Faenza <shirin.faenza@ucop.edu>
2014-06-16 11:53:49 -0700

    set shirin2 as deploy branch to test on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,8 @@ set :repo_url, 'https://auto:automaton@hg.cdlib.org/dash-ingest'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'default'
+#set :branch, 'default'
+set :branch, 'shirin2'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/apps/dash/apps/dash-ingest'
