006c632d51fccf1c1a0d62b79713bcc59d96be96
Shirin Faenza <shirin.faenza@ucop.edu>
2014-06-19 16:43:03 -0700

    rudimentary contact form , all emails to me

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,27 @@ class StaticPagesController < ApplicationController
 
   def contact
   	if request.post?
-  		GenericMailer.feedback_message.deliver
+  		 		
+  		msg = []
+      msg.push('Please enter your name') if params[:name].blank?
+      msg.push('Please enter your email') if params[:email].blank?
+      msg.push('Please enter a message') if params[:message].blank?
+      if !params[:email].blank? && !params[:email].match(/^\S+@\S+$/)
+        msg.push('Please enter a valid email address')
+      end
+      if msg.length > 0    	
+        flash.keep[:notice] = msg
+        redirect_to contact_path(name: params['name'],
+                                 email: params['email'], 
+                                 message: params[:message]) and return
+      end
+
+      receipt = "shirin.faenza@ucop.edu"
+
+      GenericMailer.feedback_message(params, receipt).deliver
+
+    	redirect_to :back, notice: "Your email message was sent to the Dash team."
+    	return
   	end
   end
 
--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -6,9 +6,27 @@ class GenericMailer < ActionMailer::Base
   #
   #   en.generic_mailer.feedback_message.subject
   #
-  def feedback_message
-    @greeting = "Hi"
-
-    mail to: "shirin.faenza@ucop.edu"
+  def feedback_message(form_hash, email)
+    @greeting = "Hello,  #{email}"
+    @form_hash = form_hash
+    mail  to: email,
+          from: @form_hash[:email],
+          subject: 'Dash Contact Us Form'
   end
 end
+
+
+
+# class GenericMailer < ActionMailer::Base
+#   default :from => APP_CONFIG['feedback_email_from']
+
+#   def contact_email(form_hash, email)
+#     # values :question_about, :name, :email, :message
+#     @form_hash = form_hash
+#     mail :to => email,
+#          :reply_to => form_hash[:email],
+#          :subject => 'DMPTool2 Contact Us Form Feedback',
+#          :from => APP_CONFIG['feedback_email_from']
+#   end
+
+# end
