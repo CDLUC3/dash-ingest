47e9af9e453cfa881aaf92b7f973d2bfcfee06b7
mark reyes <mark.reyes@ucop.edu>
2014-06-20 16:06:02 -0700

    fix merritt deployment

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -184,7 +184,7 @@ class RecordsController < ApplicationController
       Thread.new do
         @record.generate_merritt_zip
 
-        @merritt_request = @record.send_archive_to_merritt 
+        @merritt_request = @record.send_archive_to_merritt (user.external_id)
         submissionLog = SubmissionLog.new
 
         if (!@merritt_request) then
