028192a317988a003c1b41e9562fee3681bc42ee
Scott Fisher <sfisher@ucop.edu>
2014-07-09 15:22:16 -0700

    That contact form had lots of problems and needed lots of rework.  Also adding email deliver addresses to configuration file.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,58 +14,32 @@ class StaticPagesController < ApplicationController
       if !params[:email].blank? && !params[:email].match(/^\S+@\S+$/)
         msg.push('Please enter a valid email address')
       end
-      if msg.length > 0    	
-        flash.keep[:notice] = msg
-        redirect_to contact_path(name: params['name'],
-                                 email: params['email'], 
-                                 message: params[:message]) and return
-      end
 
       all_emails = APP_CONFIG['feedback_email_to'] + @campus_email #You can set the emails in config/app_config.yml
       all_emails.delete_if {|x| x.blank? } #delete any blank emails
 
-      unless params[:content].present? # honeypot check
+      if params[:content].present? || msg.length > 0 # honeypot check and validity check
+        if params[:content].present?
+          msg.push("Thanks.") #for the spam
+        end
+        flash[:notice] = msg
+      else
         all_emails.each do |email|
           GenericMailer.feedback_message(params, email).deliver
         end
+        flash[:notice] = "Your email message was sent to the Dash team."
       end
-
-    	redirect_to :back, notice: "Your email message was sent to the Dash team."
-    	return
   	
     end
   end
 
 
   def campus_email(institution)
-
-    case institution
-      when "cdl"
-        campus_email = ""
-      when "uci"
-        campus_email = ""
-       when "ucla"
-        campus_email = ""
-       when "ucsd"
-        campus_email = ""
-       when "ucsb"
-        campus_email = ""
-       when "ucb"
-         campus_email = "webman@library.berkeley.edu"
-       when "ucd"
-         campus_email = ""
-       when "ucm"
-         campus_email = ""
-       when "ucr"
-         campus_email = ""
-       when "ucsf"
-         campus_email = ""
-       when "ucsc"
-         campus_email = ""
-       else 
-         campus_email = ""
-      end
-      campus_email
+    if CAMPUS_EMAILS.has_key?(institution)
+      CAMPUS_EMAILS[institution]
+    else
+      CAMPUS_EMAILS['unknown']
+    end
   end 
 
 
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,4 +1,13 @@
-
 if File.exists?(File.join(Rails.root, 'config', 'app_config.yml'))
-  APP_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[Rails.env]
+  env = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[Rails.env]
+
+  #this if allows for indirection and have one environment reference another if it is the same then it's a simple string
+  #people were trying to use the indirection feature for the config, but it wasn't implemented here
+
+  if env.class == String
+    APP_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[env]
+  else
+    APP_CONFIG = env
+  end
+    CAMPUS_EMAILS = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))['campus_emails']
 end
