ce39ce38477f004dcc2924a54cd7d6803b48c4de
Shirin Faenza <shirin.faenza@ucop.edu>
2014-08-05 14:55:33 -0700

    refactor

--- app/models/record.rb
+++ app/models/record.rb
@@ -227,9 +227,9 @@ class Record < ActiveRecord::Base
     
      # tics will execute, for now, just print to screen
       # note that the 2>&1 is to redirect sterr to stout
-    #@user_email = DATASHARE_CONFIG['user_email_from_shibboleth']
+    @user_email = DATASHARE_CONFIG['user_email_from_shibboleth']
     #@user_email = request.headers[DATASHARE_CONFIG['user_email_from_shibboleth']]
-    @user_email = "shirin.faenza@ucop.edu"
+    #@user_email = "shirin.faenza@ucop.edu"
 
      campus = Record.id_to_campus(external_id)
      
@@ -242,15 +242,15 @@ class Record < ActiveRecord::Base
      merritt_password = MERRITT_CONFIG["merritt_#{campus}_password"]
      merritt_profile = MERRITT_CONFIG["merritt_#{campus}_profile"]
 
-    if (!@user_email.nil? && !@user_email.blank? && @user_email != [] && @user_email != "")
+    #if (!@user_email.nil? && !@user_email.blank? && @user_email != [] && @user_email != "")
 
       sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"notification=#{@user_email}\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
      
-     else
+     #else
       
-       sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
+      # sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
 
-     end
+     #end
 
      return sys_output  
 
