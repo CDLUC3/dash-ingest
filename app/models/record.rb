1c9ac6b5cd2e26f2c0b0fc66c2bbe35015fabe12
Shirin Faenza <shirin.faenza@ucop.edu>
2014-08-05 11:57:20 -0700

    fix typo

--- app/models/record.rb
+++ app/models/record.rb
@@ -241,7 +241,7 @@ class Record < ActiveRecord::Base
      merritt_password = MERRITT_CONFIG["merritt_#{campus}_password"]
      merritt_profile = MERRITT_CONFIG["merritt_#{campus}_profile"]
 
-    if @user_name
+    if @user_email
 
       sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"notification=#{@user_email}\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
      
