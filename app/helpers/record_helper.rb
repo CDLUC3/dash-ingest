c045be10b491b216aabe7cc4d696bd716205017c
Shirin Faenza <shirin.faenza@ucop.edu>
2014-08-05 11:01:52 -0700

    use mail env var from shibboleth

--- app/helpers/record_helper.rb
+++ app/helpers/record_helper.rb
@@ -32,8 +32,12 @@ DataType = {
 
 
 def user_email
-  user_email = request.headers['mail']
-  
+  #user_email = request.headers['mail']
+  # user_email = request.headers[HTTP_MAIL]
+  # if !(request.headers[HTTP_MAIL])
+  #   request.headers['HTTP_MAIL']
+  # end
+  user_email = request.headers[DATASHARE_CONFIG['user_email_from_shibboleth']]
 end
 
 
