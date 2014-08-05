6cb6b15c1a44610b493f81b217b3ed24137a8b63
Shirin Faenza <shirin.faenza@ucop.edu>
2014-08-05 15:55:34 -0700

    remove my email from code

--- app/models/record.rb
+++ app/models/record.rb
@@ -236,7 +236,7 @@ class Record < ActiveRecord::Base
     if @user
       @user_email = @user.email
     else
-      @user_email = "cinzia.faenza@gmail.com"
+      @user_email = nil
     end
 
      campus = Record.id_to_campus(external_id)
@@ -250,15 +250,11 @@ class Record < ActiveRecord::Base
      merritt_password = MERRITT_CONFIG["merritt_#{campus}_password"]
      merritt_profile = MERRITT_CONFIG["merritt_#{campus}_profile"]
 
-    #if (!@user_email.nil? && !@user_email.blank? && @user_email != [] && @user_email != "")
-
+    if @user_email.nil?
+      sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"  
+    else     
       sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"notification=#{@user_email}\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
-     
-     #else
-      
-      # sys_output = "curl --insecure --verbose -u #{merritt_username}:#{merritt_password} -F \"file=@./#{DATASHARE_CONFIG['uploads_dir']}/#{self.local_id}/#{self.local_id}.zip\" -F \"type=container\" -F \"submitter=Dash/#{external_id}\" -F \"responseForm=xml\" -F \"profile=#{merritt_profile}\" -F \"localIdentifier=#{self.local_id}\" #{merritt_endpoint} 2>&1"
-
-     #end
+    end
 
      return sys_output  
 
