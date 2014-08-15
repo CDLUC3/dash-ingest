c50524a57b6c694d322450c67df856777253f932
Rekha Meena	rekha meena <Rekha Meena	rekha.meena@ucop.edu>
2014-08-15 15:33:04 -0700

    Added testcase for edit and show  controller method

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -11,6 +11,15 @@ describe RecordsController do
     puts @user.id
   end
 
+
+  def valid_attributes
+
+    {:title => "sss", :identifierType => "nil", :identifier => "nil",  :publisher => "UC Office of the president",
+        :publicationyear =>"2014",:resourcetype => "Image,Image",:rights => "Creative Commons Attribution 4.0 International (CC-...",
+        :created_at => "2014-08-11 19:31:52",:updated_at => "2014-08-11 19:31:52",:local_id =>"uzkmimntnn", :rights_uri => "https://creativecommons.org/licenses/by/4.0/"}
+
+  end
+
   describe "GET index" do
     it "has a 200 status code" do
       get(:index, {}, {user_id: @user.id})
@@ -24,14 +33,14 @@ describe RecordsController do
     #end
   end
 
-  describe "GET new"
+  describe "GET new" do
     it "assigns a new record as @records" do
       #visit records_new_path(:id => @user.id)
       get(:new, {}, {user_id: @user.id})
       #get :new, :id => @user.id
       assigns(:record).should be_a_new(Record)
     end
-  end
+end
 
   describe "POST create" do
 
@@ -41,6 +50,9 @@ describe RecordsController do
     @record = FactoryGirl.create(:record, :title => "sss", :identifierType => "nil", :identifier => "nil",  :publisher => "UC Office of the president",
                                  :publicationyear =>"2014",:resourcetype => "Image,Image",:rights => "Creative Commons Attribution 4.0 International (CC-...",
                                  :created_at => "2014-08-11 19:31:52",:updated_at => "2014-08-11 19:31:52",:local_id =>"uzkmimntnn", :rights_uri => "https://creativecommons.org/licenses/by/4.0/")
+    @creator = FactoryGirl.create(:creator, :creatorName => "xcv")
+    @creator.save
+    @subject = FactoryGirl.create(:subject)
     @user = FactoryGirl.create(:user )
     @record.user_id = @user.id
     puts @record.id
@@ -53,8 +65,72 @@ describe RecordsController do
   end
 
   it "should create new record " do
-    post :create, {:record => @record_attributes}
+    post :create, :record => @record_atrributes, user_id: @user.id
+    assigns(:record).should be_a(Record)
+    #expect(response).to redirect_to(Record.last)
   end
+  end
+
+
+
+    describe "GET show" do
+      it "assigns the requested record as @record" do
+      @record =  Record.create! valid_attributes
+      get :show, :id => @record.id, user_id: @user.id
+      expect(response.status).to  eq(200)
+
+
+    end
+  end
+
+
+  describe "GET edit" do
+    it "assigns the requested record as @record" do
+      @record = Record.create! valid_attributes
+      get :edit, :id => @record.id, user_id: @user.id
+      assigns(:record).should be_a(Record)
+  end
+  end
+
+=begin describe "PUT update" do
+    describe "with valid params" do
+      it "updates the requested record" do
+        @record = Record.create! valid_attributes
+
+
+        Record.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, {:id => @record.id, :record => {'these' => 'params'}}, user_id: @user.id
+      end
+
+      it "assigns the requested record as @record" do
+        @record = Record.create! valid_attributes
+        put :update, {:id => @record.id, :record => valid_attributes}, user_id: @user.id
+        assigns(:record).should eq(record)
+      end
+
+      it "redirects to the record" do
+        @record = Record.create! valid_attributes
+        put :update, {:id => @record.id, :record => valid_attributes}, user_id: @user.id
+        response.should redirect_to(record)
+      end
+    end
+  end
+=end
+
+   describe 'PUT update' do
+     before :each do
+
+       @record = valid_attributes
+
+     end
+
+     context "valid attributes" do
+       it "located the requested @record" do
+         put :update, id: @record.id, record: valid_attributes, user_id: @user.id
+         assigns(:record).should eq(@record)
+       end
+     end
+   end
 
 
 end
