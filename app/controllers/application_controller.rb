3f8f1ae3c2d33b46c1a9cc17a92d7eb2af16f6de
Shirin Faenza <shirin.faenza@ucop.edu>
2014-08-15 14:45:02 -0700

    remove header-logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,27 +9,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
-  # def login
-  #   user = User.find_by_external_id(request.headers[DATASHARE_CONFIG['external_identifier']])
-    
-  #   if user.nil?
-  #     user = User.new
-  #     user.external_id = request.headers[DATASHARE_CONFIG['external_identifier']]
-  #     user.institution_id = User.institution_from_shibboleth(request.headers[DATASHARE_CONFIG['external_identifier']]).id
-  #     user.save
-  #   end
-
-  #   if user.institution_id.nil?
-  #     user.institution_id = User.institution_from_shibboleth(request.headers[DATASHARE_CONFIG['external_identifier']]).id
-  #     user.save
-  #   end
-
-  #   session[:user_id] = user.id
-  #   @current_user = user
-  #   redirect_to "/records"
-  # end
-
-
 
 end
 
