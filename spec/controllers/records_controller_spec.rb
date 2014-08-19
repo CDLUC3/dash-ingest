6818b7204bf630d722b4a64eed199df19a011871
Rekha Meena	rekha meena <Rekha Meena	rekha.meena@ucop.edu>
2014-08-19 10:54:41 -0700

    updated rspec code

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -7,7 +7,9 @@ describe RecordsController do
   before(:all) do
     @user = FactoryGirl.create(:user )
     @institution = FactoryGirl.create(:institution)
-    puts @institution.id
+    @record = FactoryGirl.create(:record)
+    @record.creators.build(FactoryGirl.attributes_for(:creator))
+    #puts @institution.id
     #puts @institution.logo
     puts @user.id
   end
@@ -44,9 +46,8 @@ describe RecordsController do
 end
 
 
-  describe "POST create" do
+   describe "POST create" do
 =begin
-    debugger
 
       it "creates a new Record" do
         expect {
@@ -57,40 +58,31 @@ end
     end
 =end
 
-
-
-
-
-
    before(:each) do
 
-    debugger
+
     @record = FactoryGirl.create(:record)
-    #@record.save
+    @record.save
     @creator = FactoryGirl.create(:creator)
     @creator.save
     @subject = FactoryGirl.create(:subject)
     #@user = FactoryGirl.create(:user )
     @record.user_id = @user.id
     puts @record.id
-    #puts @record.title
     @record.save
     @record_attributes = FactoryGirl.attributes_for(:record, :record_id => @record.id)
-    @creator_attributes = FactoryGirl.attributes_for(:creator, :record_id => @record.id)
+    @creator_attributes = @record.creators.build(FactoryGirl.attributes_for(:creator))
     @subject_attributes = FactoryGirl.attributes_for(:subject, :record_id => @record.id)
     @citation_attributes = FactoryGirl.attributes_for(:citation,:record_id => @record.id)
 
   end
   it "should create new record " do
     #post :create, :record => FactoryGirl.attributes_for(:record)
-
-    post :create, :record_id => @record.id, :record => @record_attributes, user_id: @user.id
-    assigns(:record).should be_a(Record)
+     post :create, {:record => @record_attributes}, {user_id: @user.id}
+     assigns(:record).should be_a(Record)
     #expect(response).to redirect_to(Record.last)
   end
   end
-
-
 
 
     describe "GET show" do
@@ -141,9 +133,16 @@ end
      it  "record update succeeds" do
      #before :each do
       @record = Record.create! valid_attributes
-       put :update, :id => @record.to_param
+       put :update, {:id => @record.to_param}
        assigns(:record).should eq(@record)
      end
+
+
+     #it "redirects to the record" do
+       #@record = Record.create! valid_attributes
+       #put :update, :id => @record, :record => FactoryGirl.attributes_for(:record)
+       #response.should redirect_to(record)
+     #end
 
      it "user update does not succeed" do
       @record = Record.create! valid_attributes
