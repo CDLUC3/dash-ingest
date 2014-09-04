cb55a07ad07c3bf4b0bd24f2daccc18b485c2576
sfaenza <cinzia.faenza@gmail.com>
2014-09-04 11:09:59 -0700

    refer to root_url in app_config for first part of landing page

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,4 +2,8 @@ class Institution < ActiveRecord::Base
   attr_accessible :abbreviation, :external_id_strip, :landing_page, :long_name, :short_name, :campus
 
   has_many :users
+
+
+  # ROOT_URL = ENV["root_url"]
+
 end
