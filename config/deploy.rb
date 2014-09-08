b9134f7ebc470d3888eb84ef464142783e66ff7f
sfaenza <cinzia.faenza@gmail.com>
2014-09-08 09:54:21 -0700

    set stage branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :repo_url,  'git@github.com:CDLUC3/dash-ingest.git'
 # Default branch is :master
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'master'
+#set :branch, 'master'
+set :branch, 'stage'
 #set :branch, 'joel'
 #set :branch, 'institutions'
 
