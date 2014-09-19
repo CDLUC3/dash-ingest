ec21b89b324aeee0b7b0437efce74dab2881347b
rekhaanaya <rekhu.meena@gmail.com>
2014-09-19 16:45:55 -0700

    modified omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
                            :shib_session_id_field     => "Shib-Session-ID",
                            :shib_application_id_field => "Shib-Application-ID",
-                           :debug                     =>  true,
+                           :debug                     =>  false,
 
                            :uid_field                 => "uid",
                            :name_field                => "displayName",
