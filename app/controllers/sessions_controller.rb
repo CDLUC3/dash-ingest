0c047c5e99f8e6c8e1bd92a300f765bdcfd16b23
rekhaanaya <rekhu.meena@gmail.com>
2014-09-30 09:59:39 -0700

    modified session controller to fix redirection issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_path, notice: "Signed in!"
+    redirect_to records_path, notice: "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to records_path
   end
 end
 
