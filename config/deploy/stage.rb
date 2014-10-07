01c2e86ec76ab231e6f32356b9b2f5c323b67f0b
sfaenza <cinzia.faenza@gmail.com>
2014-10-07 16:30:58 -0700

    add db to roles in cap file

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/apps/dash/apps/dash-ingest'
 
 server 'dash-stg.cdlib.org', 
 	user: 'dash', 
-	roles: %w{web app}
+	roles: %w{web app db}
 
 set :unicorn_pid, "#{deploy_to}/unicorn.pid"
 set :unicorn_config, "#{deploy_to}/current/config/unicorn.rb"
