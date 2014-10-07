d74b36c4e7a0534747c49340f20060e8ec3c1211
rekhaanaya <rekhu.meena@gmail.com>
2014-10-07 00:26:16 -0700

    code commented in login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,39 +2,39 @@ class LoginController < ApplicationController
 
   include RecordHelper
 
-  def signin
-    #logger.info "Params=#{params}"
-    if !params[:institution_id].blank?
-      session['institution_id'] = params[:institution_id]
-    end
-    @institution = Institution.find(institution)
-    session['institution_id']= @institution.id
-    if !@institution.shib_entity_domain.blank?
-      #initiate shibboleth login sequence
-      redirect_to OmniAuth::Strategies::Shibboleth.login_path_with_entity(
-                      DataIngest::Application.shibboleth_host, @institution.shib_entity_id)
-    elsif @institution.shib_entity_domain.blank?
-      redirect_to "/auth/google_oauth2"
-    end
-
-  end
-
-
-  def institution
-
-    url = request.original_url
-    if ( url == nil )
-      @id = ""
-    else
-      case url.strip
-        when /.ucop.edu/
-          @id = 1
-        else
-          @id = 12
-      end
-    end
-    return @id
-  end
+  # def signin
+  #   #logger.info "Params=#{params}"
+  #   if !params[:institution_id].blank?
+  #     session['institution_id'] = params[:institution_id]
+  #   end
+  #   @institution = Institution.find(institution)
+  #   session['institution_id']= @institution.id
+  #   if !@institution.shib_entity_domain.blank?
+  #     #initiate shibboleth login sequence
+  #     redirect_to OmniAuth::Strategies::Shibboleth.login_path_with_entity(
+  #                     DataIngest::Application.shibboleth_host, @institution.shib_entity_id)
+  #   elsif @institution.shib_entity_domain.blank?
+  #     redirect_to "/auth/google_oauth2"
+  #   end
+  #
+  # end
+  #
+  #
+  # def institution
+  #
+  #   url = request.original_url
+  #   if ( url == nil )
+  #     @id = ""
+  #   else
+  #     case url.strip
+  #       when /.ucop.edu/
+  #         @id = 1
+  #       else
+  #         @id = 12
+  #     end
+  #   end
+  #   return @id
+  # end
 
 
 
