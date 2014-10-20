4a52afa8b4c559a22141809d31a0ce8565662475
rekhaanaya <rekhu.meena@gmail.com>
2014-10-20 10:46:42 -0700

    changed shibboleth.yml to set user attributes correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,17 @@ class User < ActiveRecord::Base
 
      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
 
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      #user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.external_id = auth.info.email
-      user.institution_id = institution
-      user.save!
-      logger.debug "Params: #{user}"
+       user.provider = auth.provider
+       user.uid = auth.uid
+       user.email = auth.info.email
+       user.first_name = auth.info.first_name
+       user.last_name = auth.info.last_name
+       user.oauth_token = auth.credentials.token
+       #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+       user.external_id = auth.eppn
+       user.institution_id = institution
+       user.save!
+       logger.debug "Params: #{user}"
 
     end
    # end
