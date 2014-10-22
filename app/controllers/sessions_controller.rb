2b5d3683346b5b5e0e11394dc35f2b3705280123
Marisa Strong <marisa.strong@ucop.edu>
2014-10-21 17:23:35 -0700

    shib redirect is now constructed using the landing_page field

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -47,38 +47,43 @@ class SessionsController < ApplicationController
 
 
   def signin
-
-
-   uri = URI(request.original_url)
-   if uri.host == "localhost"
-   redirect_to sessions_create_path and return
-    else
-    @institution = Institution.find(institution)
-    session['institution_id']= @institution.id
-    if !@institution.shib_entity_domain.blank?
-      #initiate shibboleth login sequence
-      redirect_to OmniAuth::Strategies::Shibboleth.login_path_with_entity(
-                      DataIngest::Application.shibboleth_host, @institution.shib_entity_id)
-    elsif @institution.shib_entity_domain.blank?
-      redirect_to "/auth/google_oauth2"
-    end
-
-   end
-end
-
-  def institution
+    session[:user_id] = nil
+    session[:institution_id] = nil
+    cookies.delete(:dash_logged_in)
 
     uri = URI(request.original_url)
-       url = uri.host.split(".")
-       l = url.length
-       u = ".#{url[l-2]}.#{url[l-1]}"
+    if uri.host == "localhost"
+       redirect_to sessions_create_path and return
+    else
+      # grab the institution from the domain URL
+      @institution = institution
+      session['institution_id']= @institution.id
 
-      @id = Institution.find_by_landing_page(u)
-
-      return @id.id
-
+      if !@institution.shib_entity_domain.blank?
+        #initiate shibboleth login sequence
+        domain = @institution.landing_page
+        redirect_back_to_hostname = DataIngest::Application.shibboleth_host + domain
+        logger.debug "Shib Host Redirected to " + redirect_back_to_hostname
+        redirect_to OmniAuth::Strategies::Shibboleth.login_path_with_entity(
+                        redirect_back_to_hostname,
+                        @institution.shib_entity_id)
+      else
+        logger.debug "google Host Redirected to google_auth2"
+        redirect_to "/auth/google_oauth2"
+      end
+    end
   end
 
+  def institution
+    uri = URI(request.original_url)
+    url = uri.host.split(".")
+    l = url.length
+    u = ".#{url[l-2]}.#{url[l-1]}"
+
+    @institution = Institution.find_by_landing_page(u)
+    return @institution
   end
+
+end
 
 
