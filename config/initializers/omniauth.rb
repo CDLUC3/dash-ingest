4741075877bb38f1283be07361dcc3fb148345f2
rekhaanaya <rekhu.meena@gmail.com>
2014-10-27 22:35:04 -0700

    removed APP_CONFIG initialization from omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
- APP_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[Rails.env]
 
   provider :google_oauth2, APP_CONFIG['client_id'], APP_CONFIG['client_secret'], {provider_ignores_state: true}
-
-  #provider :google_oauth2, '97385814132-omb3h8so0fhsb82lr7klerm9pl8mr8is.apps.googleusercontent.com', 'TavWNTcyVkl3hUm8kvkKNGo0',{provider_ignores_state: true}
-
 
 
 end
