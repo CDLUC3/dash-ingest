43b2644f8126050d6d0459a63e7695e3305c3421
sfaenza <cinzia.faenza@gmail.com>
2014-12-10 13:31:19 -0800

    set branch uci_development as deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,11 @@ set :repo_url,  'git@github.com:CDLUC3/dash-ingest.git'
 
 
 set :branch, ENV['BRANCH'] || 'master'
-set  :filter,  :branches => %w{development, stage, master, oauth,joel,institutions}
+set  :filter,  :branches => %w{uci_development, development, stage, master, oauth,joel,institutions}
 
 #set :branch, 'master'
 #set  :branch, 'oauth'
-set :branch, 'development'
+set :branch, 'uci_development'
 
 
 
