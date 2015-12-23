ea503d1938ab91956c8b7459b8aaf052b244787a
Scott Fisher <scott.fisher@ucop.edu>
2015-12-22 17:10:25 -0800

    Turning off compression with rubyzip since it was dying on large files over about 1.6 GB each with compression.

--- config/initializers/rubyzip.rb
+++ config/initializers/rubyzip.rb
@@ -4,6 +4,6 @@ Zip.setup do |z|
   #z.on_exists_proc = true
   #z.continue_on_exists_proc = true
   #z.unicode_names = true
-  #z.default_compression = Zlib::BEST_COMPRESSION
+  z.default_compression = Zlib::NO_COMPRESSION
   z.write_zip64_support = true
 end
